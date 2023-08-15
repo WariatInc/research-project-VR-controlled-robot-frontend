72ffa9920a5090b6abcf376f054d2e8e02c58a4f
Marcel Dybowski <mdybowski20@gmail.com>
2023-08-15 13:54:45 +0200

    configuring app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'research-project-VR-controlled-robot-frontend';
+export class AppComponent implements OnInit {
+  title = 'RSWW Travel Agency App';
+  username: string | null | undefined;
+
+  constructor(private router: Router, public authService: AuthService) {}
+  ngOnInit() {
+    this.authUser();
+  }
+
+  private authUser(): void {
+    this.authService.userIsAuth();
+    this.username = localStorage.getItem('token');
+  }
+
+  public goLogin(): void {
+    this.router.navigate(['./login']);
+  }
+
+  public goReservationList(): void {
+    this.router.navigate(['./reservation-list']);
+  }
+
+  public goEventDashboard(): void {
+    this.router.navigate(['./event-dashboard']);
+  }
+
+  public logoutUser(): void {
+    this.authService.logout();
+    this.router.navigate(['./']);
+  }
+
+  public goHome(): void {
+    this.router.navigate(['./']);
+  }
 }
