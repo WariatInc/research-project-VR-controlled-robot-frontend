889c55de318ec5c09c3cf0495aae6a6d21e3f3c8
Marcel Dybowski <mdybowski20@gmail.com>
2023-08-15 13:43:30 +0200

    adding robot-list module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'robot-list', loadChildren: () => import('./robot-list/robot-list.module').then(m => m.RobotListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
